50feaad015517f50b5689c2e33803f370156a3c6
Lubna Anam <teclubnak@gmail.com>
2024-04-09 15:22:44 +0530

    jydhgc

--- referral_system_project/referral_system/tests.py
+++ referral_system_project/referral_system/tests.py
@@ -14,14 +14,14 @@ class UserTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_user_details(self):
-        user = User.objects.create(username='testuser', email='test2@example.com', password='testpass')
+        user = User.objects.create(username='testuser', email='test2@example.com', password='testpass3')
         self.client.force_authenticate(user=user)
         url = reverse('user-details')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_referrals(self):
-        user = User.objects.create(username='testuser', email='test@example.com', password='testpass', referral_code='ref123')
+        user = User.objects.create(username='testuser', email='test@example.com', password='testpass2', referral_code='ref123')
         self.client.force_authenticate(user=user)
         url = reverse('referrals')
         response = self.client.get(url)
